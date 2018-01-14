1ecbd3373b59fa4a43d060f26bb166ec08c9d457
Nicholas Baumgaertner <n.baumgaertner@gmail.com>
2018-01-14 17:36:09 +0100

    added pages

--- RCWebsite/src/js/startup.js
+++ RCWebsite/src/js/startup.js
@@ -3,5 +3,50 @@ window.onload = function () {
 }
 
 function showDevMessage() {
+    var lol = "";
+
+    loadHTML( "devMessageContent", "devMessage.html" );
+
+    // could use this if i load the full jquery script
+    // $.get("./devMessage.html", function(tasks)
+    // {
+    //    $('#devMessageContent').append(tasks);
+    // },'html');  
+
     $('#devMessageModal').modal();
 }
+
+
+function loadHTML(myDivId, url) {
+    var xmlhttp;
+    if (window.XMLHttpRequest) 
+    {
+        xmlhttp = new XMLHttpRequest();
+    } 
+    else 
+    {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlhttp.onreadystatechange = function() 
+    {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE ) 
+        {
+           if(xmlhttp.status == 200){
+               document.getElementById(myDivId).innerHTML = xmlhttp.responseText;
+               var allScripts = document.getElementById(myDivId).getElementsByTagName('script');
+               for (var n = 0; n < allScripts .length; n++)
+               {
+                   eval(allScripts [n].innerHTML)//run script inside div generally not a good idea but these scripts are anyways intended to be executed.
+               }
+           }
+           else {
+               alert('Error');
+           }
+        }
+    }
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
